85bf29e55b8e28876b3fa8bfeb194bdb60eae627
Kyle <kyle.e.cardwell@gmail.com>
2021-08-15 22:35:00 -0600

    added pointHistory to each Player

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,15 +2,25 @@ export const ADD_PLAYER = "ADD_PLAYER"
 export const TOGGLE_BANKED = "TOGGLE_BANKED"
 export const ADD_TO_POT = "ADD_TO_POT"
 export const BANK_DICE_ROLL = "BANK_DICE_ROLL"
+export const BANK_PLAYER = "BANK_PLAYER"
+export const UNBANK_PLAYER = "UNBANK_PLAYER"
 
 export const addPlayer= (player) => {
     return({type: ADD_PLAYER, payload: player})
+}
+
+export const addToPot = (num) => {
+    return({type: ADD_TO_POT, payload: num})
 }
 
 export const toggleBanked = (id, num) => {
     return({type: TOGGLE_BANKED, payload: {id: Number(id), num: Number(num)}})
 }
 
-export const addToPot = (num) => {
-    return({type: ADD_TO_POT, payload: num})
+export const bankPlayer = (id, num) => {
+    return({type: BANK_PLAYER, payload: {id: Number(id), num: Number(num)}})
+}
+
+export const unBankPlayer = (id) => {
+    return({type: UNBANK_PLAYER, payload: id})
 }

1653bce3baa54d69d22b84367f9791c7b77a8633
Kyle <kyle.e.cardwell@gmail.com>
2021-12-23 21:30:03 -0700

    reorganized flexbox items to better fill space

--- src/components/PlayersBox.js
+++ src/components/PlayersBox.js
@@ -8,11 +8,11 @@ const PlayersBox = (props) => {
     const playersList = props.players;
 
     return (
-        <section className="bg-gray-900 max-w-full rounded overflow-hidden shadow-lg p-5">
+        <div className="bg-gray-900 rounded shadow-lg p-5 grow flex flex-col flex-grow">
 
             <h4 className="font-bold text-center mb-3">Players - Click on a player's name to bank (or unbank) their score</h4>
 
-            <div className={`flex flex-wrap justify-evenly h-full`}>
+            <div className={`flex flex-wrap justify-evenly`}>
 
                 {playersList.map((person) => {
 
@@ -31,7 +31,7 @@ const PlayersBox = (props) => {
 
             </div>
 
-        </section>
+        </div>
     );
 };
 

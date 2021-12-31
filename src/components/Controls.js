3447b2f42a0234a4cfa58258b4987d3472530f6c
Kyle <kyle.e.cardwell@gmail.com>
2021-12-30 21:57:19 -0700

    removed initial players, fixed color change on banked, centered controls

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -72,7 +72,7 @@ const Controls = (props) => {
         <div className="bg-gray-900 max-w-full rounded overflow-hidden shadow-lg p-5">
 
             <div className="flex-row-reverse justify-between mb-4">
-                <h4 className="font-bold">Controls</h4>
+                <h4 className="font-bold text-center">Controls</h4>
                 <button 
                     type="button" 
                     className="border p-2 bg-white text-gray-900"
